f3650e75329212c25c3eea7afe8553685e3f2505
Stanley Hsu <sths0614@ucla.edu>
2015-12-03 04:47:48 -0800

    Finish Sender.c, Receiver.c without debugging

---
+++ Receiver.c
@@ -0,0 +1,51 @@
+#include "rdt.c"
+#include <sys/socket.h>
+
+int main(int argc, char* argv[]) 
+{
+	float pLoss, pCorrupt;
+	int portNum;
+	int sockfd;
+	struct sockaddr_in serv_addr;
+	struct hostent *server;
+	socklen_t serv_len;
+	char* fname, hostname;
+	char* fbuf;
+
+	if (argc < 6){
+		error("Wrong number or arguments, need: hostname, portNum, filename, pLoss, pCorrupt\n");
+	}
+	hostname = argv[1];
+	portNum = atoi(argv[2]);
+	fname = atoi(argv[3]);
+	pLoss = atof(argv[4]);
+	pCorrupt = atof(argv[5]);
+
+	if (portNum < 0)	
+		error("ERROR: port number must be a positive integer");
+	if (pLoss < 0.0 || pLoss > 1.0 || pCorrupt < 0.0 || pCorrupt > 1.0)
+		error("ERROR: probabilities need to be between 0.0 and 1.0");
+
+	sockfd = rdt_socket(pCorrupt, pLoss, 1);
+	if (sockfd < 0)
+		error ("ERROR from rdt_socket()");
+	server = gethostbyname(hostname);
+	if (server == NULL){
+		error ("ERROR no such host\n");
+		exit(1);
+	}
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET; //initialize server's address
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portNum);
+
+    // TODO!!!
+    int fsize = rdt_requestReply(sockfd, fname, size_t len, &fbuf, 
+    	(struct sockaddr*) serv_addr, sizeof(serv_addr));
+
+    // close the socket
+	if (rdt_close(sockfd) < 0)
+		error ("ERROR from rdt_close()");
+	return 0;
+}
---
+++ Sender.c
@@ -0,0 +1,67 @@
+#include "rdt.c"
+#include <sys/socket.h>
+
+int main(int argc, char* argv[]) 
+{
+	float pLoss, pCorrupt;
+	int portNum, winSize;
+	int sockfd;
+	struct sockaddr_in serv_addr;
+	char* fname;
+
+
+	if (argc < 5){
+		error("Wrong number or arguments, need: portNum, winSize, pLoss, pCorrupt\n");
+	}
+	portNum = atoi(argv[1]);
+	winSize = atoi(argv[2]);
+	pLoss = atof(argv[3]);
+	pCorrupt = atof(argv[4]);
+
+	if (portNum < 0)	
+		error("ERROR: port number must be a positive integer");
+	if (winSize <= 0)	
+		error("ERROR: window size must be at least 1");
+	if (pLoss < 0.0 || pLoss > 1.0 || pCorrupt < 0.0 || pCorrupt > 1.0)
+		error("ERROR: probabilities need to be between 0.0 and 1.0");
+
+	sockfd = rdt_socket(pCorrupt, pLoss, winSize);
+	if (sockfd < 0)
+		error ("ERROR from rdt_socket()");
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(portNum);
+
+	if (rdt_bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) < 0)
+		error ("ERROR from rdt_bind()");
+
+	// socklen_t zero_len = 0;
+	// rdt_recvfrom(sockfd, &fname, (struct sockaddr*) &cli_addr, &zero_len);
+	rdt_recvfrom(sockfd, &fname, (struct sockaddr*) &cli_addr, 0);
+
+	// open file, get filesize
+	FILE* f = fopen(fname, "rb");
+	fseek(f, 0L, SEEK_END);
+	int fsize = (int) ftell(f);
+	fseek(f, 0L, SEEK_SET);
+	printf("Filesize is %d\n", fsize);
+
+	// read the file into fbuf
+	char* fbuf = (char *)malloc(fsize*sizeof(char));
+	fread(fbuf, 1, fsize, f);
+
+	// close file
+	fclose(f);
+
+	// Send the file!
+	rdt_sendto(sockfd, fbuf, fsize, (struct sockaddr*) &cli_addr, sizeof(cli_addr));
+
+	// free the file buffer
+	free(fbuf);
+
+	// close the socket
+	if (rdt_close(sockfd) < 0)
+		error ("ERROR from rdt_close()");
+	return 0;
+}
