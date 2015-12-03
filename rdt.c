e2537204242a24aae55c62805dce8a3166b8de3a
zhgary <contact.avengineering@gmail.com>
2015-12-03 03:02:48 -0800

    sendto, and recvfilter tweaks

--- rdt.c
+++ rdt.c
@@ -9,6 +9,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <time.h>
 
 static char rdt_recvBuffer[2000];
 static char* rdt_sendBuffer;
@@ -17,6 +18,7 @@ static ssize_t rdt_sendBufferSize;
 static float rdt_corruptRate;
 static float rdt_lossRate;
 static ssize_t rdt_window;
+static long rdt_timeout;
 
 void error(char *msg)
 {
@@ -24,7 +26,44 @@ void error(char *msg)
     exit(1);
 }
 
-int rdt_socket(float corruptRate, float lossRate, ssize_t window)
+long timeDiff(struct timeval *tA, struct timeval *tB)
+{
+    return (tA->tv_usec + 1000000 * tA->tv_sec) - (tB->tv_usec + 1000000 * tB->tv_sec);
+}
+
+int mod256LessThan(int A, int B)
+{
+     if ((B - A) < 128 && (B - A) > 0)
+          return 1;
+     else if ((A - B) > 128)
+     {
+          return 1;
+     }
+     return 0;
+}
+
+int mod256Sub(int A, int B)
+{
+     return ((A & 0xFF) - (B & 0xFF)) & 0xFF;
+}
+
+int rdt_checkAddrMatch(const struct sockaddr* addrA, const struct sockaddr* addrB, socklen_t* addrlenA, socklen_t* addrlenB)
+{
+     if (!(addrA->sa_family == addrB->sa_family && *addrlenA == *addrlenB))
+     {
+          fprintf(stderr, "address family/length match fail\n");
+          return -1;
+     }
+     for (int i = 0; i < *addrlenA-sizeof(sa_family_t); i++)
+          if (addrA->sa_data[i] != addrB->sa_data[i])
+          {
+               fprintf(stderr, "address value match fail\n");
+               return -1;
+          }
+     return 0;
+}
+
+int rdt_socket(float corruptRate, float lossRate, ssize_t window, long timeout)
 {
      time_t t;
      srand((unsigned) time(&t));
@@ -33,8 +72,10 @@ int rdt_socket(float corruptRate, float lossRate, ssize_t window)
           rdt_corruptRate = corruptRate;
      if (corruptRate >= 0)
           rdt_lossRate = lossRate;
-     if (window > 0)
+     if (window > 0 && window < 128)
           rdt_window = window;
+     if (timeout > 0)
+          rdt_timeout = timeout;
      int sock = socket(AF_INET, SOCK_DGRAM, 0);
      fcntl(sock, F_SETFL, O_NONBLOCK);
      return sock;
@@ -48,6 +89,89 @@ int rdt_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 int rdt_close(int sockfd);
 {
      return close(sockfd);
+}
+
+ssize_t rdt_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen)
+{
+     if (len == 0)
+          return 0;
+     long packetTot = (len-1) / (PACKETSIZE-2); //total number of data packets to be sent, minus 1
+     int packetRemainder = ((len-1) % (PACKETSIZE-2) + 1);  //amount of data to be transmitted in the last packet
+     long packetNum = 0;
+     int seqNumBack = 0;
+     int seqOffset = 0;
+     struct sockaddr recvaddr;
+     socklen_t recvaddrlen;
+     char* bufC = (char*)buf;
+     char* pktBuf[PACKETSIZE];
+     struct timeval timeStamp;
+     struct timeval timeNow;
+     
+     gettimeofday(&timeStamp,NULL);
+     while (1)
+     {
+          //receive packet
+          result = lossyrecv(sockfd, (void*)pktBuf, 0, &recvaddr, &recvaddrlen);
+          if (checkAddrMatch(&recvaddr, dest_addr, &recvaddrlen, addrlen))
+          {
+               fprintf(stderr, "sendto got address mismatch\n");
+               pktBuf[0] = PACKET_DNY;
+               sendto(sockfd, pktBuf, 1, 0, &recvaddr, recvaddrlen);
+          }
+          else if (result > 0 && pktBuf[0] == PACKET_ACK) //received ACK, update window
+          {
+               int reqNum = pktBuf[1];
+               fprintf(stderr, "sendto got ack # %d\n", reqNum);
+               if (mod256LessThan(seqNumBack, reqNum))
+               {
+                    gettimeofday(&timeStamp,NULL); //reset timer
+                    int advancement = mod256sub(reqNum,seqNumBack);
+                    packetNum += advancement; //increment the number for which packet we are trying to send
+                    seqNumBack = reqNum;
+                    seqOffset -= advancement;
+               }
+          }
+          
+          
+          gettimeofday(&timeNow,NULL); //get time
+          //if timeout then mark all packets in the window as unsent
+          if (timeDiff(&timeNow,&timeStamp) >= rdt_timeout)
+          {
+               seqOffset = 0;
+          }
+          
+          if (packetNum <= packetTot) //not all packets of the message have been sent
+          {
+               
+               //send all unsent packets in the window
+               for (; seqOffset < rdt_window && (packetNum + seqOffset) <= packetTot; seqOffset++)
+               {
+                    pktBuf[0] = PACKET_DAT;
+                    pktBuf[1] = (seqNumBack + seqOffset) & 0xFF;
+                    if (packetNum == packetTot) //we are on the last packet
+                    {
+                         memcpy(pktBuf + 2, bufC + (packetNum + seqOffset) * (PACKETSIZE-2), packetRemainder);
+                         sendto(sockfd, pktBuf, packetRemainder, 0, dest_addr, addrlen);
+                    }
+                    else
+                    {
+                         memcpy(pktBuf + 2, bufC + (packetNum + seqOffset) * (PACKETSIZE-2), PACKETSIZE-2);
+                         sendto(sockfd, pktBuf, PACKETSIZE-2, 0, dest_addr, addrlen);
+                    }
+               }   
+          }
+          else if (packetNum == packetTot + 1) //all packets have been sent and ACKed, now sending confirmation of END
+          {
+               if (seqOffset != rdt_window)
+               {
+                    pktBuf[0] = PACKET_END;
+                    sendto(sockfd, pktBuf, 1, 0, dest_addr, addrlen);
+               }
+               seqOffet = rdt_window;
+          }
+          else //ACK has been received for END, exit the function
+               break;
+     }
 }
 
 ssize_t lossyrecv(int sockfd, void *buf, int block, struct sockaddr *src_addr, socklen_t* addrlen)
@@ -75,9 +199,9 @@ ssize_t lossyrecv(int sockfd, void *buf, int block, struct sockaddr *src_addr, s
                //probability to ignore
                if (rand() <= rdt_lossRate * RAND_MAX)
                {
-                    //reset variables and continue the loop if this is a blocking function call.
-                    //for non-blocking calls this results in polling for the next packet.
-                    //for blocking function calls this results in reading the next packet if it has been received or returning if the next packet has not been received.
+                    //reset variables and continue the loop.
+                    //for blocking calls this results in polling for the next packet.
+                    //for non-blocking function calls this results in reading the next packet if it has been received or returning if the next packet has not been received.
                     fprintf(stderr, "lossyrecv ignoring packet\n")
                     result = 0;
                     continue;
@@ -103,14 +227,23 @@ ssize_t rdt_recvfiltered(int sockfd, void *buf, const struct sockaddr *src_addr,
      ssize_t allocLength = 4*PACKETSIZE;
      ssize_t length = 0;
      //ssize_t msgLength = 0;
+     struct sockaddr cmpaddr;
+     socklen_t cmpaddrlen;
      struct sockaddr recvaddr;
      socklen_t recvaddrlen;
      //int lengthObtained = 0;
      int reqNum = 0;
      int invalid_addr = 0;
+     char reply[2];
      char* msgBuf = malloc(4*PACKETSIZE);
-     char* pktBuf = malloc(PACKETSIZE);
-     char ackReply[2];
+     char* pktBuf[PACKETSIZE];
+     
+     if (src_addr != NULL && addrlen != 0)
+     {
+          cmpaddr.sa_family = src_addr->sa_family;
+          cmpaddr.sa_data = src_addr->sa_data;
+          cmpaddrlen = addrlen;
+     }
      
      while (1)
      {
@@ -124,24 +257,30 @@ ssize_t rdt_recvfiltered(int sockfd, void *buf, const struct sockaddr *src_addr,
                continue;
           }
           
-          //check that address is what we want
-          int invalid_addr = 0;
-          if (!(recvaddr.sa_family == src_addr->sa_family && recvaddrlen == addrlen))
+          int packetType = pktBuf[0];
+          
+          //check that address is what we want, or if no desired address is specified, then accept the packet
+          if (cmpaddrlen != 0)
           {
-               fprintf(stderr, "recvfiltered got packet with non-matching address family or address length\n");
-               invalid_addr = 1;
-               continue;
+               if (checkAddrMatch(&recvaddr, src_addr, &recvaddrlen, addrlen))
+               {
+                    fprintf(stderr, "recvfiltered got address mismatch\n");
+                    reply[0] = PACKET_DNY;
+                    reply[1] = 0;
+                    sendto(sockfd, reply, 2, 0, &recvaddr, recvaddrlen);
+                    continue;
+               }
           }
-          int addr_match = 1;
-          for (int i = 0; i < addrlen-sizeof(sa_family_t); i++)
-               if (recvaddr.sa_data[i] != src_addr->sa_data[i])
-                    addr_match = 0;
-          if (!addr_match)
+          else if (packetType == PACKET_DAT)
           {
-               fprintf(stderr, "recvfiltered got packet with non-matching address\n");
-               invalid_addr = 1;
-               continue;
+               cmpaddr.sa_family = recvaddr.sa_family;
+               cmpaddr.sa_data = recvaddr.sa_data;
+               cmpaddrlen = addrlen;
           }
+          
+          //packet type
+          if (packetType == PACKET_END)
+               break;
           
           int seqNum = pktBuf[1];
           
@@ -149,7 +288,7 @@ ssize_t rdt_recvfiltered(int sockfd, void *buf, const struct sockaddr *src_addr,
           if (reqNum == seqNum)
           {
                fprintf(stderr, "recvfiltered got packet with seqNum %d (correct)\n", seqNum);
-               reqNum++;
+               reqNum = (reqNum + 1) & 0xFF;
           }
           else
           {
@@ -157,10 +296,9 @@ ssize_t rdt_recvfiltered(int sockfd, void *buf, const struct sockaddr *src_addr,
           }
           
           //reply with ack
-          ackReply[0] = 2;
-          ackReply[1] = reqNum;
-          sendto(sockfd, ackReply, 2, 0, src_addr, addrlen);
-          
+          reply[0] = PACKET_ACK;
+          reply[1] = reqNum;
+          sendto(sockfd, reply, 2, 0, &recvaddr, recvaddrlen);
           
           //if there is not enough space on the received message buffer, expand it
           if (allocLength - length < PACKETSIZE)
@@ -174,11 +312,6 @@ ssize_t rdt_recvfiltered(int sockfd, void *buf, const struct sockaddr *src_addr,
           //copy packet contents to buffer
           memcpy((void*)msgBuf + length, (void*)pktBuf + 2, result - 2);
           length += (result - 2);
-          
-          //determine if packet is the last packet in the sequence
-          int packetType = pktBuf[0];
-          if (packetType == PACKET_END)
-               break;
      }
      
      return length;
--- rdt.h
+++ rdt.h
@@ -15,6 +15,10 @@
 #define PACKET_DAT 0
 #define PACKET_END 1
 #define PACKET_ACK 2
+#define PACKET_DNY 3
+
+//checks if two addresses are equal
+int rdt_checkAddrMatch(const struct sockaddr* dest_addrA, const struct sockaddr* dest_addrB, socklen_t* addrlenA, socklen_t* addrlenB);
 
 //creates a UDP socket
 int rdt_socket(float corruptRate, float lossRate, int window);
@@ -29,17 +33,16 @@ int rdt_close(int sockfd);
 ssize_t rdt_sendto(int sockfd, const void *buf, size_t len, int flags, const struct sockaddr *dest_addr, socklen_t addrlen);
 
 //wrapper for recv function; simulates packet loss/corruption
-//assumes that buf is a least as large as maximum packet size
+//buf must be at least as large as maximum packet size
 ssize_t lossyrecv(int sockfd, void *buf, int block, struct sockaddr *src_addr, socklen_t* addrlen);
 
 //implements receiver side of GBN protocol. Does not return until entire message is received.
+//the function will only accept packets from the address specified in src_addr and addrlen
+//supply NULL to src_addr and 0 to addrlen to cause the function to accept packets from any address; the function will lock to the address of the first data packet it receives
 ssize_t rdt_recvfiltered(int sockfd, void *buf, const struct sockaddr *src_addr, const socklen_t addrlen);
 
 //monolithic client function. client provides buffer containing requests and the function only exits when reply has been received. recvbuf pointer is set to point to the reply. return value is the length of received buffer.
 ssize_t rdt_requestReply(int sockfd, const void* buf, size_t len, void* recvbuf, int flags, const struct sockaddr* dest_addr, socklen_t addrlen);
-
-//receives request then sets buf pointer to request data
-ssize_t rdt_waitRequest(int sockfd, void* buf);
 
 
 
